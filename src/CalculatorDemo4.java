792a2d3f365cecb5be363e39100b547b1c4ed3a8
soyeonkim0227 <soyeonkim0227@naver.com>
2022-06-01 23:23:47 +0900

    Update calculator demo
    인스턴스 = 구체적인 객체

---
+++ src/CalculatorDemo4.java
@@ -0,0 +1,32 @@
+class Calculator {
+    int left, right;
+
+    public void setOprands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void sum() {
+        System.out.println(this.left+this.right);
+    }
+
+    public void avg() {
+        System.out.println((this.left+this.right)/2);
+    }
+}
+
+public class CalculatorDemo4 {
+
+    public static void main(String[] args) {
+
+        Calculator c1 = new Calculator(); // nwe Calculator()를 통해서 인스턴스를 생성. 인스턴스는 c1이라는 변수에 담김.
+        c1.setOprands(10, 20);
+        c1.sum();
+        c1.avg();
+
+        Calculator c2 = new Calculator();
+        c2.setOprands(20, 40);
+        c2.sum();
+        c2.avg();
+    }
+}
